815299f0e47a883954cea644209cf65952415485
PULKIT\pulkit <pulkitname@gmail.com>
2023-09-08 01:32:00 +0530

    initialized

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metaData = {
+  title: "promptopia",
+  description: "Discover and share AI prompts for ChatGPT",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
