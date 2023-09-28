4c43bf1f36f2e3906ab42ec473c77159beb66eba
PULKIT\pulkit <pulkitname@gmail.com>
2023-09-28 23:12:25 +0530

    minor changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,22 @@
+"use client";
 import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href={"/"} className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="LOGO"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
